554cb84f292b8bbbdd3ecb091e8e30b6d64e21cf
developlee <developlee@163.com>
2018-07-06 23:02:42 +0800

    stampedLock

---
+++ src/main/java/com/developlee/aqs/locks/LockEg1.java
@@ -0,0 +1,62 @@
+package com.developlee.aqs.locks;
+
+import com.developlee.unThreadSafe.ConcurrencyTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Leson on 2018/7/5.
+ */
+public class LockEg1 {
+
+    private final static Lock lock = new ReentrantLock();
+
+    private final static Logger logger = LoggerFactory.getLogger(ConcurrencyTest.class);
+
+    private static Integer count = 0;
+
+    //请求总数
+    public static int clientTotal = 5000;
+
+    //同时并发执行的线程总数
+    public static int threadTotal = 200;
+
+    public static void main(String args[]) throws InterruptedException {
+        //新建一个线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //定义信号量（定义允许并发的数目）
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        //
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            //请求放入线程池内
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();//引入信号量
+                    count();
+                    semaphore.release(); //释放
+                } catch (InterruptedException e) {
+                    logger.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown(); //关闭
+        logger.info("size {}", count);
+    }
+
+    private static void count() {
+        lock.lock();
+        try {
+            count++;
+        }finally {
+            lock.unlock();
+        }
+    }
+}

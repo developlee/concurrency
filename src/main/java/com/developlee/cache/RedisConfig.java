7ab98653ac57319bfde35de9c605ed85c7fbcf89
developlee <developlee@163.com>
2018-07-15 20:10:05 +0800

    学习笔记上传

---
+++ src/main/java/com/developlee/cache/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.developlee.cache;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Leson on 2018/7/10.
+ */
+@Configuration
+public class RedisConfig {
+
+    public JRedi
+
+}
